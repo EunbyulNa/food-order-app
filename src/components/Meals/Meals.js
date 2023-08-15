840ff63f4e1653d3890d639fb8348fa49c595712
Eunbyul <las4134@gmail.com>
2023-08-15 23:56:46 +0100

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummaray from "./MealsSummary"
+import AvailableMeals from "./AvailableMeals"
+import { Fragment } from "react"
+
+const Meals = () => {
+  return <Fragment>
+    <MealsSummaray />
+    <AvailableMeals />
+  </Fragment>
+
+}
+
+export default Meals
